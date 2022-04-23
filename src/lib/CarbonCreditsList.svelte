f7f9f878f72853ea0aa900a9937a4aa4b6ab7439
klomeli <klomeli@gmail.com>
2022-04-23 17:11:14 -0400

    Added CarbonCredits component.

---
+++ src/lib/CarbonCreditsList.svelte
@@ -0,0 +1,28 @@
+<script>
+
+	let carbon_credits = [{source: "GoldStandard", serial_number: "123-abc", owner: "Alice"},
+		{source: "Verra", serial_number: "456-def", owner: "Bob"}];
+
+</script>
+
+<style>
+    fieldset {
+        border-style: solid;
+        width: 50%;
+    }
+    fieldset legend {
+        font-size: larger;
+        font-weight: bold;
+    }
+</style>
+
+<fieldset>
+	<legend>Carbon Credits</legend>
+	<ul>
+		{#each carbon_credits as carbon_credit}
+			<li>
+				{carbon_credit.source} (serial: {carbon_credit.serial_number}) - {carbon_credit.owner}
+			</li>
+		{/each}
+	</ul>
+</fieldset>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,13 @@
 <script>
 	import Accounts from '$lib/Accounts.svelte';
+	import CarbonCredits from '$lib/CarbonCreditsList.svelte';
 </script>
+
+<style>
+	div.spacer {margin-top: 20px;}
+</style>
 
 <h1>Carbon Credits Registry</h1>
 <Accounts/>
+<div class='spacer'></div>
+<CarbonCredits/>
