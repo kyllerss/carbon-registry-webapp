9a812b90a7264bfc38dcf5eb318a3287cc4f1218
klomeli <klomeli@gmail.com>
2022-04-23 17:00:50 -0400

    Added initial carbon registry main page.

---
+++ src/lib/Accounts.svelte
@@ -0,0 +1,23 @@
+<script>
+
+	let accounts = [{name: "Alice", session_id: "123"},
+									{name: "Bob", session_id: "456"}];
+
+</script>
+
+<style>
+	div {
+			border-style: solid;
+			width: 30%;
+	}
+</style>
+
+<div>
+	<ul>
+		{#each accounts as account}
+		   <li>
+				 {account.name} - {account.session_id}
+		   </li>
+		{/each}
+	</ul>
+</div>
